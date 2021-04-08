19def7a2cb6d447152ee5b473bf3edf10b225854
Guy Eisenbach <guyeise5@gmail.com>
2021-04-08 16:31:19 +0300

    new version of MainTrain

--- ex2/src/ZAnomalyDetector.scala
+++ ex2/src/ZAnomalyDetector.scala
@@ -1,7 +1,15 @@
 import scala.collection.mutable
 
 object ZAnomalyDetector extends AnomalyDetector {
-  override def learn(normal: TimeSeries): Map[String, String] = ???
+  override def learn(normal: TimeSeries): Map[String, String] = {
+    normal.features.map( e =>
+      (e._1,e._2.map(x => Math.abs(Util.zscore(e._2.toArray, x))).max.toString))
+  }
 
-  override def detect(model: Map[String, String], test: TimeSeries): Vector[(String, Int)] = ???
+  override def detect(model: Map[String, String], test: TimeSeries): Vector[(String, Int)] = {
+    val zScores = test.features.map(e => (e._1, e._2.map( x => Math.abs(Util.zscore(e._2.toArray, x)))))
+    val errors = zScores.map(e => (e._1, e._2.map(x => if (x > model(e._1).toDouble) 1 else 0)))
+    val errorIndexes = errors.map( e => (e._1, e._2.zip(e._2.indices).filter(x => x._1 == 1).map(x=> x._2)))
+    errorIndexes.map( e => (e._1, e._2.map( x => (e._1, x)))).values.reduce( (x,y) => x ++ y)
+  }
 }
