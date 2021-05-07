d6f19e0eccb21baa86db8725d425e3f8aca3b79a
Guy Eisenbach <guyeise5@gmail.com>
2021-05-07 09:37:32 +0300

    Exercise number 3

---
+++ ex3/src/Util.scala
@@ -0,0 +1,13 @@
+
+
+object Util {
+
+
+  def probs(xs:Array[Double]):Array[Double]= ???
+
+  def entropy(xs:Array[Double]):Double= ???
+
+  // you can implement additional helper methods here
+
+
+}
